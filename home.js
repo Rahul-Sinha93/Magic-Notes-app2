94235a7ce2a114c021db20c6a172916fcbe4e497
Rahul <rahulsinhar93@gmail.com>
2020-05-19 17:11:45 -0400

    Added feature to search a note

--- home.js
+++ home.js
@@ -66,3 +66,22 @@ function deleteNote(index) {
     showNotes();
 
 }
+
+let search = document.getElementById('searchTxt');
+search.addEventListener('input', function() {
+
+    let inputVal = search.value;
+    let noteCards = document.getElementsByClassName('noteCard');
+    Array.from(noteCards).forEach(function(element) {
+        let cardTxt = element.getElementsByTagName('p')[0].innerText;
+        if (cardTxt.includes(inputVal)) {
+            element.style.display = "block";
+        } else {
+            element.style.display = 'none';
+        }
+
+    });
+
+
+
+})
