b4b5d1ed73ff0dc13357ff49413e1bdda57c8434
Rahul <rahulsinhar93@gmail.com>
2020-05-19 16:14:04 -0400

    Added feature to display notes

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-console.log('hey there')
+showNotes();
 
 // if someone enters a note 
 
@@ -15,6 +15,35 @@ addBtn.addEventListener('click', function(e) {
     notesObj.push(addTxt.value);
     localStorage.setItem('notes', JSON.stringify(notesObj));
     addTxt.value = "";
-    console.log(notesObj)
+    showNotes();
 
 })
+
+function showNotes() {
+    let notes = localStorage.getItem('notes');
+    if (notes == null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
+    let html = "";
+
+    notesObj.forEach(function(element, index) {
+        html += `
+            <div class=" noteCard card my-2 mx-2" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Note ${index + 1}</h5>
+                        <p class = "card-text"> ${element}</p>
+                        <a href="#" class="btn btn-primary">Delete Note</a>
+                    </div>
+                </div>`;
+    });
+
+    let notesElm = document.getElementById('notes')
+    if (notesObj.length != 0) {
+        notesElm.innerHTML = html;
+    } else {
+        notesElm.innerHTML = `<h5> Nothing to show use : "Add Note"</h5>`
+    }
+
+}
